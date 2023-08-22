5668ed53a29f045478d586719dca6e407cbf87ae
Lam Tung Vo <37379610+nolanvo5894@users.noreply.github.com>
2023-08-22 13:49:29 -0500

    Update cancer_bot.py

--- cancer_bot.py
+++ cancer_bot.py
@@ -32,8 +32,9 @@ vectordb = Chroma(persist_directory=persist_directory,
 from langchain import PromptTemplate
 
 prompt_template = """Read the question: {question}.
-Read the context: {context}
+
 If the following pieces of context is relevant to the question, use them to answer the question. 
+Read the context: {context}
 If you don't see any relevant infomation and don't provide an answer, say you 'did not find relevant information in the database'. 
 DO NOT mention the American Cancer Society in the answer
 """
