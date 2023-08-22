b3002e6bb155325fdaea3b66b00b9285c0a79226
Lam Tung Vo <37379610+nolanvo5894@users.noreply.github.com>
2023-08-22 16:48:07 -0500

    Update cancer_bot.py

--- cancer_bot.py
+++ cancer_bot.py
@@ -35,11 +35,11 @@ from langchain import PromptTemplate
 prompt_template = """Read the question at the end.
 
 If the following pieces of context is relevant to the question, use them to answer the question at the end. 
-If you don't see any relevant infomation, make sure to say you 'did not find relevant information in the database'.
+If you don't see any relevant infomation, do not answer and make sure to say you 'did not find relevant information in the database'.
 Keep the answer less than 300 words
 
 
-{context}
+Context: {context}
 
 Question: {question}
 """
@@ -47,8 +47,8 @@ PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
 
-retriever = vectordb.as_retriever(search_kwargs={"k": 50})
-qa_chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model_name = 'gpt-3.5-turbo-16k', max_tokens=500),
+retriever = vectordb.as_retriever(search_kwargs={"k": 30})
+qa_chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model_name = 'gpt-3.5-turbo-16k', max_tokens=300),
                                                  memory=st.session_state.buffer_memory,
                                                  retriever=retriever, 
                                                  return_source_documents=True,
