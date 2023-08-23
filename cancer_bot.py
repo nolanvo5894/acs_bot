3ff0f88c5fcba9deeff816ba5c1381975a051868
Lam Tung Vo <37379610+nolanvo5894@users.noreply.github.com>
2023-08-23 09:23:00 -0500

    Update cancer_bot.py

--- cancer_bot.py
+++ cancer_bot.py
@@ -36,7 +36,7 @@ prompt_template = """Read the question at the end.
 
 Use the following pieces of context to answer the question at the end. 
 If you don't find any relevant infomation in the context, say you 'did not find relevant information in the database'.
-Keep the answer SHORT 
+
 
 
 Context: {context}
@@ -47,7 +47,7 @@ PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
 
-retriever = vectordb.as_retriever(search_kwargs={"k": 60})
+retriever = vectordb.as_retriever(search_kwargs={"k": 100})
 qa_chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model_name = 'gpt-3.5-turbo-16k', max_tokens=500),
                                                  memory=st.session_state.buffer_memory,
                                                  retriever=retriever, 
